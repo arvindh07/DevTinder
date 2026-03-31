e84f58c564530605590953d39f919f4742121d40
arvindh <arvindhganesh11@gmail.com>
2026-03-31 22:44:00 +0530

    add db mongoose connection

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { connectToDB } = require("./config/database");
+
+// load dotenv
+dotenv.config();
 
 // create app
 const app = express();
@@ -8,6 +13,19 @@ app.get("/", (req, res) => {
     return res.json("Welcome to dev tinder backend🚀");
 })
 
-app.listen(PORT, () => {
-    console.log("Server is listening on PORT", PORT, "🚀🚀");
+// Unexpected error handling
+app.use("/", (err, req, res, next) => {
+    if(err) {
+        return res.status(500).send("Internal Server Error");
+    }
+    next();
 })
+
+// connect to db
+connectToDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log("Connected to DB and Server is listening on PORT", PORT, "🚀🚀");
+        })
+    })
+    .catch((err) => console.log("Db error"))
