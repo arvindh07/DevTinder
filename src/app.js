0c6c273808442a00bd23b891bbb422c035e0016b
arvindh <arvindhganesh11@gmail.com>
2026-04-18 16:35:22 +0530

    add get, patch, delete apis for user

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { connectToDB } = require("./config/database");
-const { mongoose } = require("mongoose");
 const User = require("./models/user.model");
 
 // load dotenv
@@ -20,21 +19,20 @@ app.get("/", (req, res) => {
 })
 
 app.post("/signup", async (req, res) => {
-    const { firstName, lastName, email, password } = req.body;
-
-    // check empty validation
-    if (!firstName || !lastName || !email || !password) {
-        return res.status(400).json({ message: "Fields are required" });
-    }
-
-
-    // hash password
     try {
-        // find if the user already exists
-        const findUserByEmail = await User.find({ email });
+        const { firstName, lastName, email, password } = req.body;
 
-        if (!findUserByEmail) {
-            return res.status(400).json({ message: "User already exists" });
+        // check empty validation
+        if (!firstName || !lastName || !email || !password) {
+            return res.status(400).json({ message: "Fields are required" });
+        }
+
+        // hash password
+        // find if the user already exists
+        const findUserByEmail = await User.findOne({ email });
+
+        if (findUserByEmail) {
+            return res.status(400).json({ message: "Email already exists" });
         }
         await User.create({
             firstName,
@@ -48,6 +46,73 @@ app.post("/signup", async (req, res) => {
         })
     } catch (error) {
         console.log("Error saving the user ", error.message);
+    }
+})
+
+// get user by email
+app.post("/user", async (req, res) => {
+    try {
+        const { email } = req.body;
+        const findUser = await User.findOne({ email });
+        if (!findUser) {
+            return res.status(404).json({
+                message: "User not found"
+            })
+        }
+
+        return res.json({
+            user: findUser
+        })
+    } catch (error) {
+        console.log("Error in user api ", error.message);
+    }
+})
+
+// feed api
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        return res.json({
+            users
+        })
+    } catch (error) {
+        console.log("Error in feed api ", error.message);
+    }
+})
+
+// delete api
+app.delete("/user/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const findUser = await User.findByIdAndDelete(id);
+
+        if (findUser) {
+            return res.json({
+                message: "User deleted successfully"
+            })
+        }
+
+        return res.json({
+            message: "Already deleted"
+        })
+    } catch (error) {
+        console.log("Error in delete user api ", error.message);
+    }
+})
+
+// patch api
+app.patch("/user/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const body = req.body;
+
+        await User.findByIdAndUpdate(id, body);
+
+        return res.json({
+            message: "User updated successfully"
+        })
+    } catch (error) {
+        console.log("update error ", error.message);
     }
 })
 
